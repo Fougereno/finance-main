ee8806cde43cdc0e42379b05c50fa0b341a9057e
fougereno <fougereno@gmail.com>
2022-09-05 21:00:15 +0300

    count block changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,12 @@
 let burger = document.querySelector(".nav__burger");
+let radiowrap = document.querySelector(".main__count__radiowrap"); 
+let radio = document.querySelectorAll(".main__count__radio"); 
+let checkel = document.getElementsByClassName("main__count_checked");
 
-burger.addEventListener("click", animateburger);
+
+radio.forEach(function(element) {
+    element.addEventListener('change', changeradio)
+})
 
 function animateburger() {
     if(this.classList.contains("nav_open")) {
@@ -9,3 +15,28 @@ function animateburger() {
         this.classList.add ("nav_open");
     };
 }
+function changeradio() {
+    for (let i = 0; i < checkel.length; i++) {
+        checkel[i].classList.remove('main__count_checked');
+    }
+    if (this.checked) {
+      this.parentNode.classList.add('main__count_checked');
+    } else {
+      this.parentNode.classList.remove('main__count_checked');
+    }
+}
+
+const rangeInputs = document.querySelectorAll('input[type="range"]'); 
+
+  rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+  });
+
+  function handleInputChange(e) {
+    let target = e.target   
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'  
+  }
